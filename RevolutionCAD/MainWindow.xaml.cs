3d6010f854b8242e3ff71f8557dcef5289a0e3c8
Дмитрий Николаев <dima.nikol.99@gmail.com>
2020-02-08 19:04:40 +0300

    Формирование матриц в JSON есть, десериализацию сделаю позже

--- RevolutionCAD/MainWindow.xaml.cs
+++ RevolutionCAD/MainWindow.xaml.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Windows;
 using Microsoft.Win32;
+using System.Collections.Generic;
+//using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace RevolutionCAD
 {
@@ -78,8 +81,77 @@ namespace RevolutionCAD
 
         private void Button_CreateMatrices_Click(object sender, RoutedEventArgs e)
         {
-            // Дмитрий Николаев, в этом месте мог бы быть ваш парсер
-            // формирование матриц здесь же
+            int N = 1; // количество микросхем (первая - разъём)
+            bool IsSkipped = false; // флаг того, что ненужная часть файла пропущена
+                                    // та, которая с dip
+
+            List<string> mas = new List<string>();
+            string path = $"{ApplicationData.FileName}.sch";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line; // получаем строку из файла
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line == "#")
+                        IsSkipped = true;
+
+                    if (!IsSkipped)
+                        N++;
+                    else
+                        mas.Add(line);
+
+                }
+            }
+            mas.Remove("#"); // удаление лишнего разделителя
+            int M = mas.Count; // число проводов
+
+            Matrix<int> R = new Matrix<int>(N, N); // матрица соединений
+            Matrix<int> Q = new Matrix<int>(N, M); // матрица элементных комплексов
+
+            // обнуление матриц
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                    R[i, j] = 0;
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < M; j++)
+                    Q[i, j] = 0;
+
+            // формирование матриц
+            int numberOfContact = 0; // номер текущего провода
+            foreach (string s in mas)
+            {
+                int i = 0, j = 0;
+                try
+                {
+                    // жёсткая структура с двумя проводами и числом знаков
+                    i = int.Parse(s[1].ToString());
+                    j = int.Parse(s[8].ToString());
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Ошибка формирования матриц! Проверьте исходный файл!");
+                }
+                R[i, j]++; R[j, i]++;
+
+                Q[i, numberOfContact] = 1;
+                Q[j, numberOfContact] = 1;
+
+                numberOfContact++;
+            }
+
+            // сериализация данных в JSON
+            using (StreamWriter file = File.CreateText("R.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, R);
+            }
+            using (StreamWriter file = File.CreateText("Q.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, Q);
+            }
+            MessageBox.Show("Матрицы R и Q были удачно созданы!");
+
         }
     }
 }
